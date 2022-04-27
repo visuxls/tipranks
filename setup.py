6dbe249ace4939e4e91c995180561ca1a079aa03
Michael <michaelfitzy@me.com>
2022-04-27 14:43:17 -0500

    :bookmark: Reverted version to 0.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="tipranks",
-	version="0.0.16",
+	version="0.0.1",
 	author="visuxls",
 	description="Python interface to communicate with TipRanks API.",
 	long_description=long_description,
