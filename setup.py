ff1f6647ff28e8c285861d44c5e7fd873f1c1c7d
Michael <michaelfitzy@me.com>
2022-04-28 20:45:26 -0500

    :arrow_up: Fix version release to 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="tipranks",
-	version="0.0.2",
+	version="0.0.3",
 	author="visuxls",
 	description="Python interface to communicate with TipRanks API.",
 	long_description=long_description,
